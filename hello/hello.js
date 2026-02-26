b63f9b814fe0ad9dd3113cf5b23230b48abcf363
SamiS0830 <samanthashapiro27@marlborough.org>
2026-02-26 09:10:14 -0800

    text on screen

--- hello/hello.js
+++ hello/hello.js
@@ -1,14 +1,46 @@
+
+
+
 function setup() {
   createCanvas(400, 400);
+
+  input = createInput();
+  input.position(20, 20);
+  input.size(300);
+
+  button = createButton('enter');
+  button.position(input.x + input.width, 20);
+
+  //when button is pressed
+  button.mousePressed(formula);
 }
 
 let x = 50;
 
 function draw() {
-  background(0);
+
+  background(255);
+  
+  fill(0);
   ellipse(x, 200, 100, 50);
   x++;
 
-  if (x > width+50) 
-    x = -50;
+  textSize(32);
+  textAlign(CENTER,CENTER);
+  text(prompt, width/2, height/2);
+  
 }
+
+let prompt = "";
+
+
+function formula(){
+  //refresh background
+  
+   //connect variable to the prompt
+  prompt = input.value();
+  input.value('');
+ 
+  
+}
+
