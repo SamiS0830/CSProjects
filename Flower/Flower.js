2d3b69e62a9395f089524f278d6bf246998f7b9b
SamiS0830 <samanthashapiro27@marlborough.org>
2026-09-02 11:48:59 -0700

    flower

--- Flower/Flower.js
+++ Flower/Flower.js
@@ -1,31 +1,39 @@
 
 let Xpos= 850;
 let Ypos= 480;
-let eWidth = 10
-let eHeight = 50
-let Flowers = []
-
+let eWidth = 40
+let eHeight = 58
+let flower = [];
+let colors = ['blue','red','orange','green','yellow','purple']
+let c = 'green';
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  flower.push({x:Xpos, y:Ypos, color:'yellow'})
+  frameRate(5);
+
 }
 
 function draw() {
 
-fill('yellow');
-circle(Xpos, Ypos,30);
-fill('green');
-for(let x = 0; x < TWO_PI; x+=PI/3){
-  push();
-  translate(Xpos, Ypos)
-  rotate(x);
-  ellipse(0, -40, eWidth,eHeight);
-  pop();
+  for(let i = 0; i < flower.length; i++){
+    let f = flower[i]
+    
+    fill(c);
+    for(let x = 0; x < TWO_PI; x+=PI/3){
+      push();
+      translate(f.x, f.y)
+      rotate(x);
+     ellipse(0, -40, eWidth,eHeight);
+      pop();
+    }
+    fill(f.color);
+    circle(f.x, f.y,30);
+    c = random(colors)
+  }
 }
+function mousePressed(){
+  flower.push({x:mouseX, y:mouseY, color:'yellow'})
 }
-/*function mousePressed(){
-  Flowers.push(new Flower(mouseX, mouseY))
-}
-}
-*/
+
